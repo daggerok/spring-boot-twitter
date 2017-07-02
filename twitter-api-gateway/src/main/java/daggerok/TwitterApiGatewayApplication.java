5ca2112d7d474d0e4f1f7faebaeec5c30c4a60e5
Maksim Kostromin <daggerok@gmail.com>
2017-07-02 19:10:35 +0300

    add twitter social boilerplate:
    - stopped work with fucking twitter API (because of it's required mobile phone)
    - added all apps with backing-services into single docker-compose as twitterServicesUp twitterServicesDown
    - old task composeUp / composeDown will not work anymore! use backingServicesUp / backingServicesDown instead

---
+++ twitter-api-gateway/src/main/java/daggerok/TwitterApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package daggerok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterApiGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TwitterApiGatewayApplication.class, args);
+  }
+}
